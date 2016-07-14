0c368dcd0e31fe5730b7977c5999f47d7fb3a141
Kei Hibino <ex8k.hibino@gmail.com>
2016-07-14 15:00:27 +0900

    add definition to make request.

--- src/Data/JsonRpc/Request.hs
+++ src/Data/JsonRpc/Request.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
 
-module Data.JsonRpc.Request ( Request(..) ) where
+module Data.JsonRpc.Request (
+  Request(..), request,
+  ) where
 
 import Prelude hiding (id)
-import Data.Text (Text)
+import Data.Text (Text, pack)
 import Data.Foldable (Foldable)
 import Data.Traversable (Traversable)
 
@@ -17,3 +19,6 @@ data Request a =
   , params  :: !(Maybe a)
   , id      :: !(Maybe Id)
   } deriving (Eq, Show, Read, Functor, Foldable, Traversable)
+
+request :: Text -> Maybe a -> Maybe Id -> Request a
+request = Request $ pack "2.0"
