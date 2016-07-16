8d54e62043cc77980d983325b7ccb8dc881ffdce
Kei Hibino <ex8k.hibino@gmail.com>
2016-07-17 01:59:36 +0900

    add test cases for options.

--- test/Eq.hs
+++ test/Eq.hs
@@ -8,6 +8,7 @@ import Test.QuickCheck.Simple (Test, eqTest)
 import GHC.Generics (Generic)
 
 import Prelude hiding (id)
+import Control.Applicative ((<$>))
 import Data.Maybe (fromMaybe)
 import Data.Aeson (FromJSON)
 import qualified Data.Aeson as Aeson
@@ -16,7 +17,7 @@ import qualified Data.Aeson.Types as Aeson
 import Data.JsonRpc
   (Id(..), numberId, Request (..),
    Success, success, Failure, failure, Response (..),
-  genericParseJSONRPC, defaultJsonRpcOptions, )
+   genericParseJSONRPC, JsonRpcOptions (..), defaultJsonRpcOptions, )
 import qualified Data.JsonRpc.Failure as Failure
 
 
@@ -52,6 +53,61 @@ eqDecodeA =
                 , id = Just (NumberId 3)}
   )
 
+
+newtype FooNE = FooNE Foo deriving (Eq, Show)
+
+instance FromJSON FooNE where
+  parseJSON =
+    (FooNE <$>)
+    . genericParseJSONRPC defaultJsonRpcOptions { allowNonExistField = False } Aeson.defaultOptions
+
+eqDecodeNonExistAllow :: Test
+eqDecodeNonExistAllow =
+  eqTest "eq - decode object - allow non-exist"
+  (Aeson.decode
+   "{\"jsonrpc\": \"2.0\", \"method\": \"foo\", \"params\": { \"bar\": \"Hello\", \"foo\": 234, \"baz\": [5, 6, 7, 8], \"x\": 0 }, \"id\": 3}")
+  (Just Request { jsonrpc = "2.0"
+                , method = "foo"
+                , params = Just (Foo {foo = 234, bar = "Hello", baz = [5,6,7,8]})
+                , id = Just (NumberId 3)}
+  )
+
+eqDecodeNonExistDisallow :: Test
+eqDecodeNonExistDisallow =
+  eqTest "eq - decode object - don't allow non-exist - error"
+  (Aeson.decode
+   "{\"jsonrpc\": \"2.0\", \"method\": \"foo\", \"params\": { \"bar\": \"Hello\", \"foo\": 234, \"baz\": [5, 6, 7, 8], \"x\": 0 }, \"id\": 3}"
+  :: Maybe (Request FooNE))
+  Nothing
+
+
+newtype FooSA = FooSA Foo deriving (Eq, Show)
+
+instance FromJSON FooSA where
+  parseJSON =
+    (FooSA <$>)
+    . genericParseJSONRPC defaultJsonRpcOptions { allowSpilledArguemnts = False } Aeson.defaultOptions
+
+eqDecodeSpilledAllow :: Test
+eqDecodeSpilledAllow =
+  eqTest "eq - decode array - allow spilled args"
+  (Aeson.decode
+   "{\"jsonrpc\": \"2.0\", \"method\": \"foo\", \"params\": [ 234, \"Hello\", [5, 6, 7, 8], 0 ], \"id\": 3}")
+  (Just Request { jsonrpc = "2.0"
+                , method = "foo"
+                , params = Just (Foo {foo = 234, bar = "Hello", baz = [5,6,7,8]})
+                , id = Just (NumberId 3)}
+  )
+
+eqDecodeSpilledDisallow :: Test
+eqDecodeSpilledDisallow =
+  eqTest "eq - decode array - don't allow spilled args"
+  (Aeson.decode
+   "{\"jsonrpc\": \"2.0\", \"method\": \"foo\", \"params\": [ 234, \"Hello\", [5, 6, 7, 8], 0 ], \"id\": 3}"
+   :: Maybe (Request FooSA))
+  Nothing
+
+
 exId :: Id
 exId = fromMaybe (error "something wrong: _success") $ numberId 25
 
@@ -77,6 +133,13 @@ tests :: [Test]
 tests =
   [ eqDecodeO
   , eqDecodeA
+
+  , eqDecodeNonExistAllow
+  , eqDecodeNonExistDisallow
+
+  , eqDecodeSpilledAllow
+  , eqDecodeSpilledDisallow
+
   , eqResponseS
   , eqResponseF
   ]
