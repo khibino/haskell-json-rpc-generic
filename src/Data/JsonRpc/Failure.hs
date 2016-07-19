ef2b17e10f8933c9d1dbabaef69f9219c1aa1ad0
Kei Hibino <ex8k.hibino@gmail.com>
2016-07-19 20:04:00 +0900

    copy text from specification of json-rpc 2.0.

--- src/Data/JsonRpc/Failure.hs
+++ src/Data/JsonRpc/Failure.hs
@@ -34,6 +34,20 @@ data Error e =
   , _data    :: !(Maybe e)
   } deriving (Eq, Show, Functor, Foldable, Traversable)
 
+{-
+-- citation from http://www.jsonrpc.org/specification
+--
+-- code               message            meaning
+-- -32700             Parse error        Invalid JSON was received by the server.
+--                                       An error occurred on the server while parsing the JSON text.
+-- -32600             Invalid Request    The JSON sent is not a valid Request object.
+-- -32601             Method not found   The method does not exist / is not available.
+-- -32602             Invalid params     Invalid method parameter(s).
+-- -32603             Internal error     Internal JSON-RPC error.
+-- -32000 to -32099   Server error       Reserved for implementation-defined server-errors.
+
+-- The remainder of the space is available for application defined errors.
+ -}
 data ErrorStatus
   = ParseError
   | InvalidRequest
